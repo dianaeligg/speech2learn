4fb989ace79fcd86b2eb72fa77253fc0153d595e
Diana García <diana.eli.gg@gmail.com>
2020-07-15 00:05:20 -0500

    trying to fix heroku deployment

--- routes/api/index.ts
+++
@@ -1,7 +0,0 @@
-// const wordController = require("../../controllers/wordController");
-
-// router.use("/bcs", bcsRoutes);
-
-// router.route("/api").get(wordController.landing);
-
-// module.exports = router;
